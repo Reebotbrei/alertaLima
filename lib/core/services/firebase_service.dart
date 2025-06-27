ee53c789fe0e684382d69a6eab6d85b98697a7c5
Alvaro Mayo <neptalymayodlvega@gmail.com>
2025-06-27 09:53:41 -0500

    xddx

--- lib/core/services/firebase_service.dart
+++ lib/core/services/firebase_service.dart
@@ -1,11 +1,1 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-FirebaseFirestore baseDatos = FirebaseFirestore.instance;
-
-Future<List> getUsuarios() async {
-  List usuarios = [];
-
-  CollectionReference coleccionUsarios = baseDatos.collection("Usuario");
-  QuerySnapshot queryUsuario = await coleccionUsarios.get();
-  return usuarios;
-}
