0a456d74946ae7b4a69c50a033dee976cb1298eb
Elias Diaz <eliasalejandrodc@hotmail.com>
2025-06-28 01:40:33 -0500

    Avanzando con la seccion de mensajes

--- lib/features/chat/view/chat_page.dart
+++ lib/features/chat/view/chat_page.dart
@@ -1,12 +1,81 @@
+import 'package:alerta_lima/app/widgets/app_text_field.dart';
+import 'package:alerta_lima/features/chat/viewmodel/chat_viewmodel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
+  final TextEditingController _messageController = TextEditingController();
+  final ChatViewmodel _authService = ChatViewmodel();
+  final ChatViewmodel _chatService = ChatViewmodel();
   final String receiverEmail;
+  final String receiverID;
 
-  const ChatPage({super.key, required this.receiverEmail});
+  ChatPage({super.key, required this.receiverEmail, required this.receiverID});
+
+  void sendMessage() async {
+    if (_messageController.text.isNotEmpty) {
+      await _chatService.sendMessages(receiverID, _messageController.text);
+      _messageController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(receiverEmail)));
+    return Scaffold(
+      appBar: AppBar(title: Text(receiverEmail), centerTitle: true),
+      body: Column(
+        children: [
+          Expanded(child: _buildMessageList()),
+          _buildUserInput(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessages(receiverID, senderID),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Ocurrió un Error");
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map((doc) => _buildMessageItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return Text(data["message"]);
+  }
+
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: AppTextField(
+            controller: _messageController,
+            hintText: "Escribe un mensaje",
+            obscureText: false,
+          ),
+        ),
+
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(Icons.arrow_upward),
+        ),
+      ],
+    );
   }
 }
--- lib/features/chat/view/chat_screen.dart
+++ lib/features/chat/view/chat_screen.dart
@@ -3,15 +3,21 @@ import 'package:alerta_lima/features/chat/view/chat_user_tile.dart';
 import 'package:flutter/material.dart';
 import '../viewmodel/chat_viewmodel.dart';
 import '../../../app/theme/app_colors.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatScreen extends StatelessWidget {
-  ChatScreen({super.key});
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final ChatViewmodel _chatViewmodel = ChatViewmodel();
 
   @override
   Widget build(BuildContext context) {
-    //final viewModel = Provider.of<ChatViewModel>(context);
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(title: const Text('Contactos'), centerTitle: true),
@@ -33,7 +39,7 @@ class ChatScreen extends StatelessWidget {
 
         return ListView(
           children: snapshot.data!
-              .map<Widget>((userData) => _buildUserListItem(userData, context))
+              .map<Widget>((userData) => _buildUserListItem(userData))
               .toList(),
         );
       },
@@ -42,18 +48,38 @@ class ChatScreen extends StatelessWidget {
 
   Widget _buildUserListItem(
     Map<String, dynamic> userData,
-    BuildContext context,
   ) {
-    return UserTile(
-      text: userData['Email'],
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ChatPage(
-            receiverEmail: userData['Email'],
-          )),
-        );
-      },
-    );
+    if ((userData['Email'] ?? "Usuario sin correo") !=
+        ChatViewmodel().getCurrentUser()!.email) {
+      return UserTile(
+        text: userData['Email'] ?? "Usuario sin correo",
+        onTap: () async {
+          String receiverID1 = await obtenerID(userData['Email']);
+          if (!mounted) return;
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverEmail: userData['Email'] ?? "Usuario sin correo",
+                receiverID: receiverID1,
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Future<String> obtenerID(String email) async {
+    QuerySnapshot querySnapshot = await _firestore.collection('Usuario').get();
+
+    for (var doc in querySnapshot.docs) {
+      if (doc['Email'] == email) {
+        return doc.id;
+      }
+    }
+    return 'AjBEShVMNBR0ckJRt8A1HPiL2zn1';
   }
 }
